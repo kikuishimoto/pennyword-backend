316ef219249b4f6d1e88d1c5938002c28f7a3a25
millardjimmy <millardjimmy@gmail.com>
2020-08-27 10:27:24 -0600

    add flashcard table

---
+++ db/migrate/20200827162321_create_flashcards.rb
@@ -0,0 +1,11 @@
+class CreateFlashcards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :flashcards do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
